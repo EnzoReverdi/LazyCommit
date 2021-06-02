c37991a2ce7f55cac1d518f4c33b8fb5bb58456d
EnzoReverdi <enzo.reverdi@epitech.eu>
2021-06-02 05:05:10 +0200

    Base checking done

--- LazyCommit.sh
+++ LazyCommit.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
+
 DIR=$PWD
+STATUSREPO=$(git status)
 
 main () {
+    cd $DIR
+
+    if [[ ! -d .git ]]
+    then
+        echo "Your are not in the root of your repository, operation aborted"
+        exit 1
+    fi
+
     if [[ -n "$1" ]]; then
         if [[ "$1" != "-a"]]; then
             echo "BAD ARGUMENT OPERATION ABORTED"
+            exit 1
+        else
+            if [[ $STATUSREPO == *"Changes to be committed"* ]]; then
+            else
+                echo "You didn't add file to commit, aborting operation"
+                exit 1
+            fi
         fi
     else
-        git add --all
+        if [[ $STATUSREPO == *"Changes not staged for commit:"* ]]; then
+            git add --all
+        else
+            echo "Nothing to commit, abborting operation"
+            exit 1
+        fi
     fi
+
 }
 
 main $@
